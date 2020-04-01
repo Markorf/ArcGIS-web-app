f38428196aec76518381e5b15c215c298ed6b5ea
Markorf <marko.medic59@gmail.com>
2020-04-02 00:04:27 +0200

    Dodate male izmene

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,43 +2,53 @@ require([
   "esri/Map",
   "esri/views/SceneView",
   "esri/widgets/BasemapToggle",
-  "esri/widgets/BasemapGallery"
-], (Map, SceneView, BasemapToggle, BasemapGallery) => {
+  "esri/widgets/BasemapGallery",
+  "esri/widgets/Search",
+  "esri/layers/GraphicsLayer",
+  "esri/widgets/Sketch"
+], (
+  Map,
+  SceneView,
+  BasemapToggle,
+  BasemapGallery,
+  Search,
+  GraphicsLayer,
+  Sketch
+) => {
   const opcije = {
     stanja: {
-      prikaziGaleriju: true,
-      prikaziDonjuMapu: true,
+      prikaziDesnuSekciju: true,
+      prikaziDonjuSekciju: true,
       lat: 45.38361,
       long: 20.38194,
       udaljenost: 5000,
-      rotacija: 30
+      rotacija: 30,
+      kontrolerPrikazan: true
     },
     elementi: {
-      prikazGalerije: document.querySelector("#prikazGalerije"),
-      prikaziDonjuMapu: document.querySelector("#prikazDonjeMape"),
+      header: document.querySelector("header"),
+      prikazDesneSekcije: document.querySelector("#prikazDesneSekcije"),
+      prikazDonjeSekcije: document.querySelector("#prikazDonjeSekcije"),
       prikazLokacije: document.querySelector("#prikazLokacije"),
       prikazUdaljenosti: document.querySelector("#prikazUdaljenosti"),
       promenaRotacije: document.querySelector("#promenaRotacije"),
-      forma: document.querySelector("#forma")
+      togluj: document.querySelector("#togluj")
     },
     metode: {
-      prikazGalerije() {
-        this.stanja.prikaziGaleriju = !this.stanja.prikaziGaleriju;
+      prikazDesneSekcije() {
+        this.stanja.prikaziDesnuSekciju = !this.stanja.prikaziDesnuSekciju;
         start();
       },
-      prikazDonjeMape() {
-        this.stanja.prikaziDonjuMapu = !this.stanja.prikaziDonjuMapu;
+      prikazDonjeSekcije() {
+        this.stanja.prikaziDonjuSekciju = !this.stanja.prikaziDonjuSekciju;
         start();
       },
       prikaziMojuLokaciju() {
         if (navigator.geolocation) {
-          const [latInput, longInput] = [
-            ...this.elementi.forma.querySelectorAll("input")
-          ];
           navigator.geolocation.getCurrentPosition(pozicija => {
-            latInput.value = pozicija.coords.latitude;
-            longInput.value = pozicija.coords.longitude;
-            this.metode.promeniLokaciju.call(this);
+            this.stanja.lat = pozicija.coords.latitude;
+            this.stanja.long = pozicija.coords.longitude;
+            start();
           });
         } else {
           alert("Geolokacija nije podrzana");
@@ -52,24 +62,26 @@ require([
         this.stanja.rotacija = event.target.value;
         start();
       },
-      promeniLokaciju() {
-        const [latInput, longInput] = [
-          ...this.elementi.forma.querySelectorAll("input")
-        ];
-        this.stanja.lat = +latInput.value;
-        this.stanja.long = +longInput.value;
-        start();
+      toglujKontrole() {
+        this.stanja.kontrolerPrikazan = !this.stanja.kontrolerPrikazan;
+        if (this.stanja.kontrolerPrikazan) {
+          this.elementi.togluj.innerHTML = "Sakrij kontrole";
+          this.elementi.header.classList.remove("zatvori");
+        } else {
+          this.elementi.togluj.innerHTML = "Prikazi kontrole";
+          this.elementi.header.classList.add("zatvori");
+        }
       }
     },
 
     init() {
-      this.elementi.prikazGalerije.addEventListener(
+      this.elementi.prikazDesneSekcije.addEventListener(
         "change",
-        this.metode.prikazGalerije.bind(this)
+        this.metode.prikazDesneSekcije.bind(this)
       );
-      this.elementi.prikaziDonjuMapu.addEventListener(
+      this.elementi.prikazDonjeSekcije.addEventListener(
         "change",
-        this.metode.prikazDonjeMape.bind(this)
+        this.metode.prikazDonjeSekcije.bind(this)
       );
       this.elementi.prikazLokacije.addEventListener(
         "click",
@@ -83,21 +95,23 @@ require([
         "change",
         this.metode.promeniRotaciju.bind(this)
       );
-      this.elementi.forma.addEventListener("submit", event => {
-        event.preventDefault();
-        this.metode.promeniLokaciju.call(this);
-      });
+      this.elementi.togluj.addEventListener(
+        "click",
+        this.metode.toglujKontrole.bind(this)
+      );
     }
   };
 
   opcije.init();
 
-  const start = () => {
-    const map = new Map({
-      basemap: "topo-vector",
-      ground: "world-elevation"
-    });
+  const graphicsLayer = new GraphicsLayer();
 
+  const map = new Map({
+    basemap: "topo-vector",
+    ground: "world-elevation",
+    layers: [graphicsLayer]
+  });
+  const start = () => {
     const view = new SceneView({
       container: "viewDiv",
       map,
@@ -109,6 +123,10 @@ require([
         },
         tilt: opcije.stanja.rotacija // perspective (stapeni)
       }
+    });
+
+    const search = new Search({
+      view
     });
 
     const basemapToggle = new BasemapToggle({
@@ -126,15 +144,20 @@ require([
       }
     });
 
-    if (opcije.stanja.prikaziDonjuMapu) {
+    const sketch = new Sketch({
+      view: view,
+      layer: graphicsLayer
+    });
+
+    // dodavanje opcija
+
+    if (opcije.stanja.prikaziDonjuSekciju) {
       view.ui.add(basemapToggle, "bottom-right");
-    } else {
-      view.ui.remove(basemapToggle, "bottom-right");
+      view.ui.add(sketch, "bottom-left");
     }
-    if (opcije.stanja.prikaziGaleriju) {
+    if (opcije.stanja.prikaziDesnuSekciju) {
+      view.ui.add(search, "top-right");
       view.ui.add(basemapGallery, "top-right");
-    } else {
-      view.ui.remove(basemapGallery, "top-right");
     }
   };
 
